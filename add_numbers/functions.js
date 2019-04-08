5ad444e81af1ba319746ea4240f90f270b5add41
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-08 18:34:15 +0100

    passes 'second parameter must be a number' test

--- add_numbers/functions.js
+++ add_numbers/functions.js
@@ -1,5 +1,5 @@
 function add(num1, num2) {
-    if (typeof num1 !== 'number') {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
         return 'NaN';
     } else {
         return num1 + num2;  
