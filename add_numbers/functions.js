e40806fdab221d529a357ec6dbf3c03976dc65f8
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-08 18:14:56 +0100

    passes 'first parameter must be a number' test

--- add_numbers/functions.js
+++ add_numbers/functions.js
@@ -1,4 +1,8 @@
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 !== 'number') {
+        return 'NaN';
+    } else {
+        return num1 + num2;  
+    }
 }
 module.exports = add;
