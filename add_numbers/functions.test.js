7e418c227f7efeb3f6aeb01c4db90c10d873c443
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-08 17:55:06 +0100

    fails test for first param to be a number

--- add_numbers/functions.test.js
+++ add_numbers/functions.test.js
@@ -6,9 +6,9 @@ test('returns a value', function () {
 
 test('adds 3 + 4 to equal 7', function () {
     expect(add(3, 4)).toBe(7);
-})
+});
 
-// test('both parameters must be numbers', function () {
-//     expect(add('3', [5,4])).toBe('both parameters must be of type - number');
-// });
+test('first parameter must be number', function () {
+    expect(add('3', 4)).toBe('NaN');
+});
 
