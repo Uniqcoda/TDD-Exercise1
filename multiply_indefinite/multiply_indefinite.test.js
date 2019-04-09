61ca7f558f5594dff5ada27305b7af20f47fc62c
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 15:18:54 +0100

    write and fail test that all parameters must be numbers

--- multiply_indefinite/multiply_indefinite.test.js
+++ multiply_indefinite/multiply_indefinite.test.js
@@ -2,11 +2,16 @@
 
 const multiply = require('./multiply_indefinite');
 
+
+
 test('multiply 3 * 5 to equal 15', function () {
     expect(multiply(3, 5)).toBe(15);
 });
 
-
 test('Multiply an indefinite amount of arguments', function () {
     expect(multiply(3, 2, 5)).toBe(30);
+});
+
+test('parameters must be numbers', function () {
+    expect(multiply(3, 2, true)).toBe('NaN');
 });
