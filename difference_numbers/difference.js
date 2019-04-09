2e223fb0176135ae308c73a8c2ea701bf4638b2a
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 10:22:45 +0100

    passes test for difference of NaN

--- difference_numbers/difference.js
+++ difference_numbers/difference.js
@@ -1,5 +1,9 @@
 function difference(num1, num2) {
-    return num1 - num2;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'NaN';
+    } else {
+        return num1 - num2;  
+    }
 };
 
 module.exports = difference;
