fea26fdddf2b5ef94d106d26bb5fb74919272d5b
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 18:50:12 +0100

    pass test to concatenate two strings

--- concatenation/concatenation.js
+++ concatenation/concatenation.js
@@ -1,5 +1,5 @@
 // Takes two strings as arguments and return the concatenation of the two strings.
-function concatenate() {
-    
+function concatenate(str1, str2) {
+    return str1.concat(str2)
 };
  module.exports = concatenate;
