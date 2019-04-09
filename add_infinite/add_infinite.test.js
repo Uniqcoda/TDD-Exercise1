6435a3d0562c9cfa8fc3fac88c4b86a57f834b42
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 10:56:17 +0100

    fails test to add 4 numbers

--- add_infinite/add_infinite.test.js
+++ add_infinite/add_infinite.test.js
@@ -2,4 +2,8 @@ const add = require('./add_infinite');
 
 test('add (3, 4, 2) to equal 9 ', function () {
     expect(add(3, 4, 2)).toBe(9);
+});
+
+test('add (3, 4, 2, 1) to equal 10 ', function () {
+    expect(add(3, 4, 2, 1)).toBe(10);
 })
