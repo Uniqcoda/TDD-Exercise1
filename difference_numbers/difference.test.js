7c1c2fc47579702abbf9a102135e9813138177b5
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 10:20:37 +0100

    wrote and failed test for difference of NaN

--- difference_numbers/difference.test.js
+++ difference_numbers/difference.test.js
@@ -2,7 +2,10 @@
 
 const difference = require('./difference');
 
-test('substracts 10 -3 to equal 7', function () {
+test('substracts 10 - 3 to equal 7', function () {
     expect(difference(10, 3)).toBe(7);
 });
 
+test('both parameter must be numbers', function () {
+    expect(difference(3, '1')).toBe('NaN');
+});
