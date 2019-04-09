255605dc62367eaaac06ac417e9159f797ad600a
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 13:46:58 +0100

    pass test to multiply an indefinite amount of arguments

--- multiply_indefinite/multiply_indefinite.js
+++ multiply_indefinite/multiply_indefinite.js
@@ -1,5 +1,9 @@
-function multiply(num1, num2) {
-    return num1 * num2;
+function multiply() {
+    var m = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+            m *= arguments[i];
+    }
+    return m;
 };
 
 module.exports = multiply;
