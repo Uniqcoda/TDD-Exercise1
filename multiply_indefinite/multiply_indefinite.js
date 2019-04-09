13886f5e471d56052d2ff4ba9a61e4ffe12cd032
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 15:39:39 +0100

    pass test that all parameters must be numbers

--- multiply_indefinite/multiply_indefinite.js
+++ multiply_indefinite/multiply_indefinite.js
@@ -1,7 +1,11 @@
 function multiply() {
     var m = arguments[0];
     for (let i = 1; i < arguments.length; i++) {
-            m *= arguments[i];
+        if (typeof arguments[i] !== 'number') {
+            return 'NaN';
+        } else {
+            m *= arguments[i];  
+        }
     }
     return m;
 };
