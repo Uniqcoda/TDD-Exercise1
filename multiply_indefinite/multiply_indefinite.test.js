f79c7f5cd9bf33f6329cf60d39730e58bd82f271
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 13:41:23 +0100

    fail test to multiply an indefinite amount of arguments

--- multiply_indefinite/multiply_indefinite.test.js
+++ multiply_indefinite/multiply_indefinite.test.js
@@ -6,3 +6,7 @@ test('multiply 3 * 5 to equal 15', function () {
     expect(multiply(3, 5)).toBe(15);
 });
 
+
+test('Multiply an indefinite amount of arguments', function () {
+    expect(multiply(3, 2, 5)).toBe(30);
+});
