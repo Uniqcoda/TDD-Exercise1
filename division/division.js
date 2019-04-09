2de0dc5898e0dbb6ab81873df6ed54bf384eb6ea
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 16:58:31 +0100

    pass first test to divide

--- division/division.js
+++ division/division.js
@@ -1,6 +1,6 @@
 // Takes two numbers as arguments and returns the first number divided by the second number.
-function divide() {
-    
+function divide(num1, num2) {
+    return num1/num2;
 };
 
 module.exports = divide;
