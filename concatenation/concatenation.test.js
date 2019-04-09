243cf9cccf59fe3693eb44f287f70179334ed03d
Mary Etokwudo <etokwudomary@gmail.com>
2019-04-09 19:13:17 +0100

    write and fail test for 'both param are stings'

--- concatenation/concatenation.test.js
+++ concatenation/concatenation.test.js
@@ -1,6 +1,10 @@
 // Takes two strings as arguments and return the concatenation of the two strings.
 const concatenate = require('./concatenation');
 
-test('', function () {
+test("concatenate 'Hello ' and 'Mary!' to give 'Hello Mary!'", function () {
     expect(concatenate('Hello ', 'Mary!')).toBe('Hello Mary!');
+});
+
+test('both parameters must be strings', function () {
+    expect(concatenate(5, 'Mary!')).toBe('both parameters must be strings');
 });
